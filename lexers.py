3b2da9dfc4aa196a3bde504147c91baf13b124fd
Istasis Mishra <istasis.mishra@research.iiit.ac.in>
2022-11-07 16:11:08 +0530

    Add support for Nexus files, debug find and save, refactor

--- lexers.py
+++ lexers.py
@@ -18,11 +18,30 @@ class FastaLexer(QsciLexerCustom):
         super(FastaLexer, self).__init__(parent)
 
         # style 1
-        self.setColor(QColor("#639e3b"), 1)  # green
+        self.setColor(QColor("#444444"), 1)
         self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 1)
 
+        # style for A
+        self.setColor(QColor("#4BA619"), 2)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 2)
+
+        # style for T
+        self.setColor(QColor("#ff0000"), 3)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 3)
+
+        # style for C
+        self.setColor(QColor("#0000ff"), 4)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 4)
+
+        # style for G
+        self.setColor(QColor("#C411C7"), 5)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 5)
+
         self.rules = [
-            # From '>' until a newline
+            (re.compile(r"[Aa]", re.MULTILINE), 2),
+            (re.compile(r"[Tt]", re.MULTILINE), 3),
+            (re.compile(r"[Cc]", re.MULTILINE), 4),
+            (re.compile(r"[Gg]", re.MULTILINE), 5),
             (re.compile(r"^>[^\n]*", re.MULTILINE), 1),  # style 1
         ]
 
@@ -107,9 +126,11 @@ class PhylipLexer(QsciLexerCustom):
         self.seq_length = int(metadata[1])
 
         self.rules = [
-            (re.compile(r"^.+ ", re.MULTILINE), 1),
-            (re.compile(r"^[0-9]+ [0-9]+$", re.MULTILINE), 2),
+            [re.compile(r"^.+ ", re.MULTILINE), 1],
+            (re.compile(r'^[0-9]+ [0-9]+$', re.MULTILINE), 2),
         ]
+
+        self.type = None
 
     def language(self):
         return "Phylip"
@@ -121,7 +142,7 @@ class PhylipLexer(QsciLexerCustom):
 
         """
         Classify Phylip style as:
-        
+
         1. Strict Sequential (SS)
         2. Strict Interleaved (SI)
         3. Relaxed Sequential (RS)
@@ -133,10 +154,9 @@ class PhylipLexer(QsciLexerCustom):
 
         lines = text.strip().split('\n')
 
-
         # TODO: handle all text, not just visible on screen
 
-        if len(lines)-1 == self.num_species:
+        if len(lines) - 1 == self.num_species:
             # Sequential Format
 
             # Check for lengths in each sequence
@@ -152,12 +172,84 @@ class PhylipLexer(QsciLexerCustom):
 
         print('type', self.type)
 
-
     def styleText(self, start, end):
 
         text = self.parent().text()[start:end]
 
         phylip_type = self.classify_type(text)
+
+        highlights = []
+        for rule in self.rules:
+            highlights += [
+                (m.start(0), len(bytearray(m.group(0), "utf-8")), rule[1])
+                for m in re.finditer(rule[0], text)
+            ]
+
+        for highlight in highlights:
+            self.startStyling(start + highlight[0])
+            self.setStyling(highlight[1], highlight[2])
+
+
+class NexusLexer(QsciLexerCustom):
+
+    def __init__(self, parent):
+        super(NexusLexer, self).__init__(parent)
+
+        # style 1
+        self.setColor(QColor("#639e3b"), 1)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 1)
+
+        # style for A
+        self.setColor(QColor("#4BA619"), 2)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 2)
+
+        # style for T
+        self.setColor(QColor("#ff0000"), 3)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 3)
+
+        # style for C
+        self.setColor(QColor("#0000ff"), 4)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 4)
+
+        # style for G
+        self.setColor(QColor("#C411C7"), 5)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 5)
+
+        # style for Comments
+        self.setColor(QColor("#888888"), 6)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE), 6)
+
+        # style for keywords
+        self.setColor(QColor("#5103C1"), 7)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 7)
+
+        # style for keywords
+        self.setColor(QColor("#7A1919"), 8)
+        self.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE, weight=QFont.Bold), 8)
+
+        self.rules = [
+            (re.compile(r"[A]", re.MULTILINE), 2),
+            (re.compile(r"[T]", re.MULTILINE), 3),
+            (re.compile(r"[C]", re.MULTILINE), 4),
+            (re.compile(r"[G]", re.MULTILINE), 5),
+            (re.compile(r"[N]", re.MULTILINE), 6),
+            (re.compile(r"(#NEXUS|end|;)", re.MULTILINE | re.IGNORECASE), 3),
+            (re.compile(r"\=\{?[0-9A-Z_!@#$*?-]+\}?", re.MULTILINE), 2),
+            (re.compile(r"^(begin).*;", re.MULTILINE | re.IGNORECASE), 3),
+            (re.compile(r"(dimensions|format|matrix|charset|outgroup)", re.MULTILINE | re.IGNORECASE), 7),
+            (re.compile(r"(ntax|nchar|datatype|gap|missing|interleave)", re.MULTILINE | re.IGNORECASE), 8),
+            (re.compile(r"^\[.*\]", re.MULTILINE), 6),
+
+        ]
+
+    def language(self):
+        return "Nexus"
+
+    def description(self, style):
+        return f"style_{style}"
+
+    def styleText(self, start, end):
+        text = self.parent().text()[start:end]
 
         highlights = []
         for rule in self.rules:
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys, os
-import subprocess 
+import subprocess
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -13,9 +13,9 @@ from PyQt5.QtWidgets import (
 import platform
 
 from PyQt5.QtCore import *
-from PyQt5.QtGui import QIcon, QFont, QColor, QBrush
-from PyQt5.QtWidgets import (QApplication, QWidget , QInputDialog,
-                             QRadioButton, QLineEdit, QDialogButtonBox, QFormLayout)
+from PyQt5.QtGui import QIcon, QFont, QColor, QBrush, QWheelEvent
+from PyQt5.QtWidgets import (QApplication, QWidget, QInputDialog, QRadioButton,
+                             QLineEdit, QDialogButtonBox, QFormLayout)
 
 from PyQt5.uic import loadUiType
 from PyQt5.Qsci import QsciScintilla
@@ -27,20 +27,23 @@ import re
 
 FORM_CLASS, _ = loadUiType("ui/main.ui")
 
+
 class FindInputDialog(QDialog):
-    def __init__(self, parent=None):
+    def __init__(self, file_ext, parent=None):
         super().__init__(parent)
 
         self.text = QLineEdit(self)
-        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self);
+        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
         self.radioChoice1 = QRadioButton(self)
         self.radioChoice1.setText('Find in sequence names')
         self.radioChoice2 = QRadioButton(self)
         self.radioChoice2.setText('Find in DNA sequences')
 
+        if file_ext not in ['fastq', 'fas']:
+            self.radioChoice1.hide()
+            self.radioChoice2.hide()
         layout = QFormLayout(self)
         layout.addRow("Find:", self.text)
-
         layout.addWidget(self.radioChoice1)
         layout.addWidget(self.radioChoice2)
         layout.addWidget(buttonBox)
@@ -49,15 +52,16 @@ class FindInputDialog(QDialog):
         buttonBox.rejected.connect(self.reject)
 
     def getInputs(self):
-        return (self.text.text(), self.radioChoice1.isChecked(), self.radioChoice2.isChecked())
+        return self.text.text(), self.radioChoice1.isChecked(), self.radioChoice2.isChecked()
+
 
 class SequenceEditor(QMainWindow, FORM_CLASS):
     resized = pyqtSignal()
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Sequence Editor")
         self.setWindowIcon(QIcon("ui/resources/itaxo.png"))
-        #self.showMaximized()
         self.path = "Untitled"
         self.txt = ""
         self.setWindowTitle(os.path.basename(self.path))
@@ -66,7 +70,7 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
         self.connectTriggers()
 
         self.value = 0
-        #ScrollBar for lazy loading
+        # ScrollBar for lazy loading
         self.scroll = QScrollBar(self)
         scroll_x = 780
         scroll_y = 500
@@ -74,20 +78,29 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
         self.scroll.valueChanged.connect(lambda: self.scroll_text())
         self.scroll.setFocusPolicy(Qt.WheelFocus)
 
-        
-
         self.find = []
+        self.find_pos = []
         self.find_string = ""
         self.find_count = 0
 
         # Editor
         self.editor.setUtf8(True)
         self.editor.setFont(QFont(config.DEFAULT_FONT, config.FONT_SIZE))
+        self.editor.setAutoCompletionSource(QsciScintilla.AcsDocument)
 
-        self.editor.setMarginType(1, QsciScintilla.NumberMargin)
-        self.editor.setMarginWidth(1, 30)
+        # self.editor.setMarginType(3, QsciScintilla.NumberMargin)
+        self.editor.setMarginWidth(0, 30)
         self.editor.setMarginsForegroundColor(QColor(120, 128, 120))
-        self.editor.setMarginLineNumbers(1, True)
+        self.editor.setMarginLineNumbers(0, True)
+
+        self.editor.setCaretLineVisible(True)
+        self.editor.setCaretLineBackgroundColor(QColor("#e6e6e6"))
+
+        self.editor.setIndentationsUseTabs(False)
+        self.editor.setTabWidth(4)
+        self.editor.setIndentationGuides(True)
+        self.editor.setTabIndents(True)
+        self.editor.setAutoIndent(True)
 
         self.LEXERS = {
             "md": lexers.MarkdownLexer,
@@ -97,15 +110,16 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
             "fas": lexers.FastaLexer,
             "fsa": lexers.FastaLexer,
             "fastq": lexers.FastqLexer,
-            "nex": lexers.PythonLexer,
-            "nxs": lexers.PythonLexer,
+            "nex": lexers.NexusLexer,
+            "nxs": lexers.NexusLexer,
             "phy": lexers.PhylipLexer,
         }
         self.setLexer(self.LEXERS["py"])
 
         # search indicator
-        self.SEARCH_INDICATOR_ID = 1
+        self.SEARCH_INDICATOR_ID = 16
         self.editor.indicatorDefine(QsciScintilla.FullBoxIndicator, self.SEARCH_INDICATOR_ID)
+        self.editor.indicatorDefine(QsciScintilla.BoxIndicator, self.SEARCH_INDICATOR_ID + 1)
 
         # Enable/Disable find_next and find_prev
         self.FIND_ACTIVE = False
@@ -113,11 +127,14 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
         # Keep track of text wrapping
         self.WRAP_MODE = 'unwrapped'
 
+    def wheelEvent(self, event):
+        self.scroll.setValue(int(self.value - event.angleDelta().y() / 20))
+
     def scroll_text(self):
         buffer_text = self.editor.text()
         buffer_arr = buffer_text.split("\n")
         buffer_arr = buffer_arr[:-1]
-        buffer_arr = [ i + "\n" for i in buffer_arr]        
+        buffer_arr = [i + "\n" for i in buffer_arr]
         self.txt[self.value:self.value+(int(self.geometry().height()/20))] = buffer_arr
         self.value = self.scroll.value()
         self.editor.setText(''.join(self.txt[self.value:self.value+(int(self.geometry().height()/20))]))
@@ -142,24 +159,23 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
         self.action_Find_Prev.triggered.connect(self.find_prev)
         self.action_Find_Next.triggered.connect(self.find_next)
         self.action_Replace.triggered.connect(self.replace)
-        #self.action_Find_Down.triggered.connect(self.find_down)
+        # self.action_Find_Down.triggered.connect(self.find_down)
         self.action_About.triggered.connect(self.help_about)
         self.resized.connect(self.ScrollBarPosition)
 
-
-    #Signal For Screen Size Change
+    # Signal For Screen Size Change
     def resizeEvent(self, event):
-            self.resized.emit()
-            return super(SequenceEditor, self).resizeEvent(event)
+        self.resized.emit()
+        return super(SequenceEditor, self).resizeEvent(event)
 
-    #Change ScrollBar with screen size
+    # Change ScrollBar with screen size
     def ScrollBarPosition(self):
         width = self.geometry().width()
         height = self.geometry().height()
         self.scroll.setGeometry(width-20, 60, 20, height-100)
         value = self.scroll.value()
         self.editor.setText(''.join(self.txt[value:value+(int(self.geometry().height()/20))]))
-        
+
 
     def edit_wrap_text(self):
         if self.WRAP_MODE == 'unwrapped':
@@ -170,7 +186,8 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
             self.WRAP_MODE = 'unwrapped'
 
     def find_search(self):
-        dialog = FindInputDialog()
+        file_ext = self.path.split(".")[-1]
+        dialog = FindInputDialog(file_ext=file_ext)
 
         outputs = ('', False, False)
 
@@ -184,7 +201,7 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
         name = outputs[0]
         os_name = platform.system()
         try:
-            if( os_name == "Linux"):
+            if os_name == "Linux":
                 output = str(subprocess.check_output('grep -n ' + str(name) + ' ' + self.path, shell=True))[2:-1]
             else:
                 t_path = self.path
@@ -194,57 +211,54 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
             # Not found
             self.dialog_message("Search string not found")
             return
+        tmp_x = output.replace("\\r", "").split("\\n")
 
-        tmp_x = output.split("\\r\\n") 
+        if len(tmp_x) == 0:
+            # Not found
+            self.dialog_message("Search string not found")
+            self.FIND_ACTIVE = False
+            return
 
         sequence_names = []
         dna_sequences = []
+        pattern_occurances = []
 
-
-        file_ext = self.path.split(".")[-1]
         for i in tmp_x:
             try:
                 start = i.split(':')[1]
-                if file_ext == 'fastq' and (start[0] == '+' or start[0] == '@'):
-                    sequence_names.append(i)
-                if file_ext == 'fas' and (start[0] == '>'):
-                    sequence_names.append(i)
-                else:
-                    dna_sequences.append(i)
+                if file_ext == 'fastq':
+                    if start[0] == '+' or start[0] == '@':
+                        sequence_names.append(i)
+                elif file_ext == 'fas':
+                    if start[0] == '>':
+                        sequence_names.append(i)
+                    else:
+                        dna_sequences.append(i)
+                pattern_occurances.append(i)
             except:
                 dna_sequences.append(i)
                 sequence_names.append(i)
+                pattern_occurances.append(i)
 
-        # 0 for sequence names, 1 for dna sequences
-        self.search_option = 0
+        # -1 for text, 0 for sequence names, 1 for dna sequences
+        self.search_option = -1
 
         x = []
-        if outputs[1]:
+        if not outputs[1] and not outputs[2]:
+            x += pattern_occurances
+        elif outputs[1]:
+            self.search_option = 0
             x += sequence_names
-        if outputs[2]:
+        elif outputs[2]:
             self.search_option = 1
             x += dna_sequences
-
-        BL = [i.split(":")[0] for i in x][:-1]
-        self.find = [int(i) for i in BL]
-
-        if len(self.find) == 0:
-            # Not found
-            self.dialog_message("Search string not found")
-            return
+        self.find = [int(i.split(":")[0]) for i in x[:-1]]
+        self.find_pos = [int(i.split(":")[1].find(self.find_string)) for i in x[:-1]]
 
         self.find_count = 0
 
-        buffer_text = self.editor.text()
-        buffer_arr = buffer_text.split("\n")
-        buffer_arr = buffer_arr[:-1]
-        buffer_arr = [ i + "\n" for i in buffer_arr]        
-        self.txt[self.value:self.value+(int(self.geometry().height()/20))] = buffer_arr
-
-        self.scroll.setValue(self.find[self.find_count]-1)
-        self.value = self.scroll.value()
-        self.editor.setText(''.join(self.txt[self.value:self.value+(int(self.geometry().height()/20))]))
-
+        self.scroll.setValue(self.find[self.find_count] - 1)
+        self.scroll_text()
         current_window_size_max = self.value+(int(self.geometry().height()/20))
         self.clear_highlight_util(current_window_size_max)
         self.find_highlight_util(current_window_size_max)
@@ -275,8 +289,8 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
             return
 
         repl, done2 = QInputDialog.getText(
-        self, 'Input Dialog', 'Replace:')
-        self.txt[self.value] = self.txt[self.value].replace(self.find_string,repl)
+            self, 'Input Dialog', 'Replace:')
+        self.txt[self.value] = self.txt[self.value].replace(self.find_string, repl)
         self.editor.setText(''.join(self.txt[self.value:self.value+(int(self.geometry().height()/20))]))
 
     def find_next(self):
@@ -285,9 +299,11 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
 
         self.find_count = self.find_count + 1
         try:
-            self.scroll.setValue(self.find[self.find_count]-1)
-            self.value = self.scroll.value()
-            self.editor.setText(''.join(self.txt[self.value:self.value+(int(self.geometry().height()/20))]))
+            if self.find[self.find_count] >= self.value + int(self.geometry().height() / 20) or \
+                    self.find[self.find_count] <= self.value:
+                self.scroll.setValue(self.find[self.find_count]-1)
+                self.value = self.scroll.value()
+                self.editor.setText(''.join(self.txt[self.value:self.value + (int(self.geometry().height()/20))]))
         except:
             self.find_count = 0
             self.scroll.setValue(self.find[self.find_count]-1)
@@ -300,30 +316,34 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
 
     def clear_highlight_util(self, window_max):
         self.editor.clearIndicatorRange(min(self.value - 5, 0), 0, window_max+1, 0, self.SEARCH_INDICATOR_ID)
+        self.editor.clearIndicatorRange(min(self.value - 5, 0), 0, window_max+1, 0, self.SEARCH_INDICATOR_ID + 1)
 
     def find_highlight_util(self, window_max):
         offset = 1
-        if self.search_option == 1:
+        if self.search_option == 0:
             offset = 0
 
+        self.editor.fillIndicatorRange(self.find[self.find_count] - offset - self.value,
+                                       self.find_pos[self.find_count],
+                                       self.find[self.find_count] - offset - self.value,
+                                       self.find_pos[self.find_count] + len(self.find_string),
+                                       self.SEARCH_INDICATOR_ID + 1)
+
         for line in self.find:
-            if line-2 >= self.value - 5 and line-2 <= window_max + 5:
-                print(line)
-                # print('line sample', self.txt[line-1][:10])
-                for match in re.finditer(self.find_string, self.txt[line-1]):
-                    self.editor.fillIndicatorRange(line-2-offset, 0, line-1-offset,
-                            0, self.SEARCH_INDICATOR_ID)
-                    # print('**', match.start(), match.end())
+            if len([re.finditer(self.find_string, self.txt[line - 1])]):
+                self.editor.fillIndicatorRange(line - offset - self.value, 0,
+                                               line - offset - self.value + 1, 0,
+                                               self.SEARCH_INDICATOR_ID)
 
     def file_open(self):
         path, _ = QFileDialog.getOpenFileName(
             parent=self, caption="Open file", filter=config.FILTER_TYPES
         )
         try:
-            
+
             #text = open(path , "r").read()
-            text = open(path , "r").readlines()
-            self.scroll.setMaximum(len(text))            
+            text = open(path, "r").readlines()
+            self.scroll.setMaximum(len(text))
             self.txt = text
             #self.editor.setText(text)
             self.editor.setText(''.join(text[0:(int(self.geometry().height()/20))]))
@@ -340,19 +360,20 @@ class SequenceEditor(QMainWindow, FORM_CLASS):
         if self.path == "Untitled":
             self.file_saveAs()
             return
-
+        self.scroll_text()
         try:
-            text = self.editor.text()
+            text = ''.join(self.txt)
             with open(self.path, "w") as f:
                 f.write(text)
         except Exception as e:
             self.dialog_message(str(e))
 
     def file_saveAs(self):
+        self.scroll_text()
         path, _ = QFileDialog.getSaveFileName(
             parent=self, caption="Save file as", filter=config.FILTER_TYPES
         )
-        text = self.editor.text()
+        text = ''.join(self.txt)
 
         try:
             with open(path, "w") as f:
